146c75216bd44546a36173599e8f84c2a7833038
Joleyne Hernandez Hernandez 12th FHHS <128279@SWCSD.US>
2024-02-01 13:07:21 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        addingNumbers(5,6);
+        addingNumbers(5,6.4,7);
+
+
+
+
+
+    }
+    static void addingNumbers (int A, int B ){
+        System.out.println(A+B);
+    }
+    static void addingNumbers (int A, Double B, int C ){
+        System.out.println(A+B+C);
+    }
+}
